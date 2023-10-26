5ea9da41b1baf49196e29906ea889db552bd21b5
Carles Pregonas <carles.prego@gmail.com>
2023-10-26 18:27:57 +0200

    Testing text combination with color
    Similar artist is just for not changing Kyryl's code.
    Test Denoise is for checking the quality of the noise filter.
    Text combination compares the artist found and comparing in the BBDD artists list.
    Main Carles includes those scripts on main

---
+++ Week3/similar_artist.py
@@ -0,0 +1,52 @@
+import Levenshtein
+import pandas as pd
+from pathlib import Path
+import pytesseract
+import re
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+
+class SimilarArtist:
+    def __init__(self, path_query_csv):
+        self.query_path = path_query_csv
+
+    def most_similar_string(self, target, ref_set):
+        min_distance = float('inf')
+        most_similar = None
+
+        ref_set = {row["idx"]: row["artist"] for _, row in ref_set.iterrows()}
+        for candidate in ref_set.values():
+            distance = Levenshtein.distance(target, candidate)
+            if distance < min_distance:
+                min_distance = distance
+                most_similar = candidate
+
+        #print(f"The most similar string to '{target}' is: '{most_similar}'")
+        return most_similar
+
+
+    def text_similarities(self, text):
+        ref_set = pd.read_csv(self.query_path)
+
+        return self.most_similar_string(text, ref_set)
+
+    def valid_text(self, text):
+        clean_text = text.replace(" ", "")
+        return len(clean_text) > 0
+
+    def __call__(self, image, bbox_coords):
+        x, y, w, h = bbox_coords
+        image = image[y: y + h, x: x + w]
+        text = pytesseract.image_to_string(image)
+        text = re.sub(r'[^a-zA-Z\s]', '', text)
+        return self.text_similarities(text) if self.valid_text(text) else [None]
+
+
+if __name__ == "__main__":
+    target_string = "Ponce"
+    REF_CSV_PATH = Path(r"C:\Users\maria\PycharmProjects\C1_CVC\data\Week3\paintings_db_w2d1.csv")
+    ref_set = pd.read_csv(REF_CSV_PATH)
+    Similar_Artist = SimilarArtist(REF_CSV_PATH)
+
+    most_similar = Similar_Artist.most_similar_string(target_string, ref_set)
+
---
+++ Week3/text_combination.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from pathlib import Path
+
+
+class CompareArtist:
+    def __init__(self, path_csv_bbdd):
+        self.bbdd_path = path_csv_bbdd
+
+    def get_artist_bbdd(self, artist):
+        ref_set = pd.read_csv(self.bbdd_path, encoding='ISO-8859-1')
+        ref_set = {row["idx"]: row["artist"] for _, row in ref_set.iterrows()}
+
+        return [key for key, value in ref_set.items() if value == artist]
+
+    def compare_artist(self, results: dict, artists: dict):
+        compared_result = []
+        for idx, result in results.items():
+            if artists[idx] is [None]:
+                compared_result.append(result)
+                continue
+            ref_idx = self.get_artist_bbdd(artists[idx])
+
+            correct_artist = []
+            not_correct_artist = []
+            for item in result:
+                if item in ref_idx:
+                    correct_artist.append(item)
+                else:
+                    not_correct_artist.append(item)
+            compared_result.append(correct_artist + not_correct_artist)
+
+        return compared_result
+
+    def compare_multiple_paintings_artist(self, results: dict, artists: dict):
+        compared_results = []
+        for idx, result in results.items():
+            compared_result = []
+            for i, artist in enumerate(artists[idx]):
+                if artist is [None]:
+                    compared_result.append(result[i])
+                    continue
+                ref_idx = self.get_artist_bbdd(artist)
+                correct_artist = []
+                not_correct_artist = []
+                for item in result[i]:
+                    if item in ref_idx:
+                        correct_artist.append(item)
+                    else:
+                        not_correct_artist.append(item)
+                compared_result.append(correct_artist + not_correct_artist)
+            compared_results.append(compared_result)
+
+        return compared_results
+
+
+
